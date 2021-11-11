16454386274550ff9d84e4f2fefef8548d1e32e1
Thuany26 <thuanydesouza26@gmail.com>
2021-11-10 22:36:57 -0300

    delete

--- PrimeiraAPI/index.js
+++ PrimeiraAPI/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const routes = require('./config/routes')
 
 const app = express()
 
@@ -10,30 +10,7 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors())
-
-
-let db = [
-    { '1': { Nome: 'Cliente 1', Idade: '20'} },
-    { '2': { Nome: 'Cliente 2', Idade: '20'} },
-    { '3': { Nome: 'Cliente 3', Idade: '20'} }
-]
-
-
-//Buscar dados
-app.get('/', (req, res) => {
-    return res.json(db)
-})
-
-//Inserir dados
-app.post('/add', (req, res) => {
-    const body = req.body
-
-    if(!body)
-        return res.status(400).end()
-
-    db.push(body)
-    return res.json(body)
-})
+app.use(routes)
 
 app.listen(21626, () => {
     console.log('Express started at http://localhost:21626')
